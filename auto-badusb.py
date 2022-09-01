e8fedbd2b78496458c7395cd83064d95354a070e
TeALO36 <83779026+TeALO36@users.noreply.github.com>
2022-09-01 17:45:36 +0200

    Add files via upload

--- auto-badusb.py
+++ auto-badusb.py
@@ -4,7 +4,7 @@ import time
 os.remove("data.txt")
 fichier = open("data.txt", "a")
 fichier.write(
-    '#include <Keyboard.h> \nconst int BUTTON_PIN = 3; \nint lastState = HIGH; \nint currentState; \nString fr(String text){\n  int i = 0;\n  String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()",\n         _fr = " =azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN,;:!?./ & \\"\'(- _  1234567890",\n         str = "";\n  while (text[i] != 0){\n    str = str + (String)_en[_fr.indexOf((String)text[i++])];\n  }\n  return str;\n}\nvoid setup() { \n   Serial.begin(9600); \n   Keyboard.begin();\n   delay(1000);\n   pinMode(BUTTON_PIN, INPUT_PULLUP); \n} \nvoid loop() { \n  currentState = digitalRead(BUTTON_PIN); \n  if(lastState == LOW && currentState == HIGH){ \n   Serial.println("1"); \n ')
+    '#include <Keyboard.h> \nconst int BUTTON_PIN = 3; \nint lastState = HIGH; \nint currentState; \nString fr(String text){\n  int i = 0;\n  String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()",\n         _fr = " =azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN,;:!?./ & \\"\'(- _  1234567890",\n         str = "";\n  while (text[i] != 0){\n    str = str + (String)_en[_fr.indexOf((String)text[i++])];\n  }\n  return str;\n}\nvoid setup() { \n   Serial.begin(9600); \n   Keyboard.begin();\n   delay(1000);\n   pinMode(BUTTON_PIN, INPUT_PULLUP); \n} \nvoid loop() { \n  currentState = digitalRead(BUTTON_PIN); \n  if(lastState == LOW && currentState == HIGH){ \n   Serial.println("1"); \n   Keyboard.press(KEY_LEFT_GUI); \n   Keyboard.press(\'d\'); \n   delay(10); \n   Keyboard.releaseAll(); \n')
 help = '\n     "open" pour executer un programme \n     "text" pour ecrire du text \n     "download" pour télécharger un fichier depuis un liens http.s \n     "file" pour importer un fichier texte a écrire sur le pc\n '
 print(help)
 
@@ -49,7 +49,7 @@ def download():
 
 def file():
     fichier.write(
-        f'   Keyboard.press(KEY_LEFT_GUI); \n   Keyboard.press(\'r\'); \n   delay(10); \n   Keyboard.releaseAll(); \n   delay(200); \n   Keyboard.print(fr("notepad")); \n   Keyboard.press(KEY_RETURN); \n   delay(100); \n   Keyboard.releaseAll();\n ')
+        f'   Keyboard.press(KEY_LEFT_GUI); \n   Keyboard.press(\'r\'); \n   delay(10); \n   Keyboard.releaseAll(); \n   delay(200); \n   Keyboard.print(fr("notepad")); \n   Keyboard.press(KEY_RETURN); \n   delay(100); \n   Keyboard.releaseAll();\n   delay(200);\n')
 
     infile = input(
         "Quel est le nom du fichier a ouvrir ? ( extension doit être inclu, le fichier gardera le nom et l'extension par defaut ): ")
@@ -100,7 +100,7 @@ while True:
 
 
 fichier.write(
-    '   } else { \n    Serial.println("0"); \n   }\nlastState = currentState;\n} \n')
+    '   Keyboard.press(KEY_LEFT_GUI); \n   Keyboard.press(\'d\'); \n   delay(100); \n   Keyboard.releaseAll(); \n   } else { \n    Serial.println("0"); \n   }\nlastState = currentState;\n} \n')
 fichier.close()
 print("Le fichier a bien été enregistré sous le nom de data.txt")
 time.sleep(10)
